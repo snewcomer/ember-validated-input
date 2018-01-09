0c4a5b8d1da1790daabf7b57dec9aacaa642d5d9
snewcomer24 <snewcomer24@gmail.com>
2018-01-09 07:22:12 -0800

    upgrade to 2.18

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,37 +1,37 @@
 import Controller from '@ember/controller';
 import { action } from 'ember-decorators/object';
 
-import {
-  validatePresence
-} from 'ember-changeset-validations/validators';
+// import {
+//   validatePresence
+// } from 'ember-changeset-validations/validators';
 
-const NEW_MODEL = {
-  title: validatePresence(true),
-  name: validatePresence(true)
-}
+// const NEW_MODEL = {
+//   title: validatePresence(true),
+//   name: validatePresence(true)
+// }
 
 export default class App extends Controller {
-  model = {
-    title: '',
-    name: ''
-  }
+  // model = {
+  //   title: '',
+  //   name: ''
+  // }
   
-  validations = NEW_MODEL;
+  // validations = NEW_MODEL;
 
-  /**
-   * called when hitting enter or clicking button
-   * applies underlying changes to data model 
-   * 
-   * @method validate
-   * @param {Object} changeset 
-   */
-  @action
-  validate(changeset) {
-    changeset.validate();
-    if (changeset.get('isValid')) {
-      changeset.execute();
-    }
-  }
+  // /**
+  //  * called when hitting enter or clicking button
+  //  * applies underlying changes to data model 
+  //  * 
+  //  * @method validate
+  //  * @param {Object} changeset 
+  //  */
+  // @action
+  // validate(changeset) {
+  //   changeset.validate();
+  //   if (changeset.get('isValid')) {
+  //     changeset.execute();
+  //   }
+  // }
 
   @action
   submitEvent() {}
